22cdc96fb1f8014eb59dcbb8ca5153c50dbb3e35
vlad0-0 <54848408+vlad0-0@users.noreply.github.com>
2019-10-08 14:19:43 +0300

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,8 @@
+print("проверка условия a < b и b < c")
+a = int(input("Введите a: "))
+b = int(input("Введите b: "))
+c = int(input("Введите c: "))
+if a < b < c:
+	print("True")
+else:
+	print("False")
